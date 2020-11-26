3e25190c96e8821eca92b9857f1496f849409c0a
cxhello <1061594797@qq.com>
2020-11-26 16:19:30 +0800

    Update nacos-spring-cloud-config-example.

---
+++ nacos-spring-cloud-config-example/src/main/java/com/cxhello/example/controller/ConfigController.java
@@ -0,0 +1,24 @@
+package com.cxhello.example.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author caixiaohui
+ * @date 2020/11/26
+ */
+@RestController
+@RequestMapping("/config")
+@RefreshScope
+public class ConfigController {
+
+    @Value("${useLocalCache:false}")
+    private boolean useLocalCache;
+
+    @RequestMapping("/get")
+    public boolean get() {
+        return useLocalCache;
+    }
+}
